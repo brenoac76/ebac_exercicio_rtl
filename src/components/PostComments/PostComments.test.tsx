a253bc8622b0261f2b9c23da8b19e30082cf8398
Breno Andrade Costa <breno.ac@gmail.com>
2024-06-01 21:20:53 -0300

    Atualização de arquivos de teste

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Adicionar dois comentários', () => {
+        render(<PostComment/>);
+
+        const addComment = (text: string) => {
+            fireEvent.change(screen.getByTestId('comment-textarea'), {
+                target: {
+                    value: text,
+                }
+            });
+            fireEvent.click(screen.getByTestId('comment-button'));
+        };
+
+        addComment('Comentário 1');
+        addComment('Comentário 2');
+        
+
+        expect(screen.getAllByTestId('comment-text')).toHaveLength(2);
     });
 });
